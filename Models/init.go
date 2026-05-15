7f5c3175c8455d3f7fff7c3c2d19ae3fa9c0d51a
wseeee <msnq5wgcqt@privaterelay.appleid.com>
2026-05-15 11:18:30 +0800

    ip2

--- Models/init.go
+++ Models/init.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"time"
 
 	"github.com/go-redis/redis/v8"
 	"gorm.io/driver/mysql"
@@ -30,15 +31,23 @@ func Init() *gorm.DB {
 	dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8mb4&parseTime=True&loc=Local",
 		user, password, host, port, dbname)
 
-	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
-	if err != nil {
-		log.Printf("Failed to connect to database: %v", err)
+	var db *gorm.DB
+	var err error
+	for i := 0; i < 30; i++ {
+		db, err = gorm.Open(mysql.Open(dsn), &gorm.Config{})
+		if err == nil {
+			log.Println("Database connected successfully")
+			return db
+		}
+		log.Printf("Database not ready (attempt %d/30): %v", i+1, err)
+		time.Sleep(2 * time.Second)
 	}
-	return db
+	log.Fatalf("Failed to connect to database after 30 attempts: %v", err)
+	return nil
 }
 
 func InitRedisDB() *redis.Client {
-	host := getEnv("REDIS_HOST", "192.168.64.128")
+	host := getEnv("REDIS_HOST", "127.0.0.1")
 	port := getEnv("REDIS_PORT", "6379")
 	password := getEnv("REDIS_PASSWORD", "123456")
 
@@ -47,5 +56,15 @@ func InitRedisDB() *redis.Client {
 		Password: password,
 		DB:       0,
 	})
+
+	for i := 0; i < 10; i++ {
+		if err := rdb.Ping(rdb.Context()).Err(); err == nil {
+			log.Println("Redis connected successfully")
+			return rdb
+		}
+		log.Printf("Redis not ready (attempt %d/10), retrying...", i+1)
+		time.Sleep(2 * time.Second)
+	}
+	log.Println("Warning: Redis connection could not be established")
 	return rdb
 }
